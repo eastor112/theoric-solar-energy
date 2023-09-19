480ff339d256abc217f52cb0e6fa783430e26cd7
emerson asto <emerar.mct@gmail.com>
2023-09-18 23:09:01 -0500

    fixed bad case and variables names

--- energ_anual.py
+++ energ_anual.py
@@ -18,10 +18,10 @@ azimuth = 180               # Angulo de azimuth del colector, gamma [deg] (180 m
 # DATOS DEL TUBO AL VACIO (CONFIGURACION GEOMÉTRICA)
 # ===========================================
 
-D_int = 0.048   # Diametro interno del tubo al vacio [m]
-D_ext = 0.058   # Diametro externo del tubo al vacio [m]
-L_tubo = 1.80   # Longitud efectivo del tubo al vacio expuesto al sol [m]
-S_sep = 0.116   # Distancia de separacion entre centro de tubos [m]
+d_int = 0.048   # Diametro interno del tubo al vacio [m]
+d_ext = 0.058   # Diametro externo del tubo al vacio [m]
+l_tubo = 1.80   # Longitud efectivo del tubo al vacio expuesto al sol [m]
+s_sep = 0.116   # Distancia de separacion entre centro de tubos [m]
 
 
 # CONFIGURACION DE LA MATRIZ HORARIA
@@ -32,42 +32,42 @@ nn = 121        # Division de puntos que abarca todo el dia
 print("RESULTADOS DE ENERGIA DIARIA APROVECHADA EN 1 TUBO AL VACIO")
 print("============================================================\n")
 
-Energia_Diaria = np.zeros(365)
+energia_diaria = np.zeros(365)
 
 for n in range(1, 366):
-    Dia = n
+  Dia = n
 
-    Energia_Total_1T = get_pipe_energy(
-      n,
-      latitud_local,
-      inclinacion,
-      azimuth,
-      longitud_local,
-      nn,
-      altitud_local,
-      D_int,
-      D_ext,
-      S_sep,
-      L_tubo
-    )
-    Energia_Diaria[n - 1] = Energia_Total_1T
+  Energia_Total_1T = get_pipe_energy(
+    n,
+    latitud_local,
+    inclinacion,
+    azimuth,
+    longitud_local,
+    nn,
+    altitud_local,
+    d_int,
+    d_ext,
+    s_sep,
+    l_tubo
+  )
+  energia_diaria[n - 1] = Energia_Total_1T
 
 for n in range(1, 365):
-    if np.isinf(Energia_Diaria[n]) or np.isnan(Energia_Diaria[n]):
-        Energia_Diaria[n] = (Energia_Diaria[n - 1] + Energia_Diaria[n + 1]) / 2
+    if np.isinf(energia_diaria[n]) or np.isnan(energia_diaria[n]):
+        energia_diaria[n] = (energia_diaria[n - 1] + energia_diaria[n + 1]) / 2
 
-Dia_col = np.arange(1, 366).reshape(-1, 1)  # Vector que estima el dia
-Energia_Diaria_col = Energia_Diaria.reshape(-1, 1)
+dia_col = np.arange(1, 366).reshape(-1, 1)  # Vector que estima el dia
+energia_diaria_col = energia_diaria.reshape(-1, 1)
 
-Energia_Anual = np.sum(Energia_Diaria)  # Valor total de energia anual aprovechada en esa condición
+energia_anual = np.sum(energia_diaria)  # Valor total de energia anual aprovechada en esa condición
 
 import matplotlib.pyplot as plt
 
 plt.figure(1)
-plt.plot(Dia_col, Energia_Diaria_col)
+plt.plot(dia_col, energia_diaria_col)
 plt.title("Energia Diaria Disponible en 1 Tubo al Vacio [kW-h]")
 plt.xlabel("Dia")
 plt.ylabel("Energia [kW-h]")
 plt.show()
 
-print("Energia Anual Aprovechada en 1 Tubo al Vacio:", Energia_Anual, "kW-h")
+print("Energia Anual Aprovechada en 1 Tubo al Vacio:", energia_anual, "kW-h")
--- energ_tubo.py
+++ energ_tubo.py
@@ -10,14 +10,14 @@ def get_pipe_energy(
   longitud_local,
   nn,
   altitud_local,
-  D_int,
-  D_ext,
-  S_sep,
-  L_tubo
+  d_int,
+  d_ext,
+  s_sep,
+  l_tubo
 ):
   # CALCULOS PUNTUALES INSTANTÁNEOS
   # ===============================
-  Gon = m.extraterrestrial_radiation(n)
+  gon = m.extraterrestrial_radiation(n)
   ang_delta = m.declination_angle(n)
   ang_omega_s = m.sunset_hour_angle(latitud_local, ang_delta)
   hora_aparec_sol = m.sunrise(ang_omega_s)
@@ -25,69 +25,67 @@ def get_pipe_energy(
 
   # CALCULOS ANGULARES HORARIOS (manteniendo desde "hora_aparec_sol" hasta "hora_puesta_sol")
   # ==========================
-  HoraSol = np.zeros(nn)
-  HoraStd = np.zeros(nn)
-  AngOmega = np.zeros(nn)
-  AngTheta = np.zeros(nn)
-  AngThetaZ = np.zeros(nn)
+  hora_sol = np.zeros(nn)
+  hora_std = np.zeros(nn)
+  ang_omega = np.zeros(nn)
+  ang_theta = np.zeros(nn)
+  ang_theta_z = np.zeros(nn)
 
   for i in range(nn):
-      HoraSol[i] = hora_aparec_sol + (hora_puesta_sol - hora_aparec_sol) / nn * (i - 1)
-      HoraStd[i] = m.standard_time(n, HoraSol[i], longitud_local)
-      AngOmega[i] = m.hour_angle(HoraSol[i])
-      AngTheta[i] = m.incidence_angle(ang_delta, latitud_local, inclinacion, azimuth, AngOmega[i])
-      AngThetaZ[i] = m.zenith_angle(ang_delta, latitud_local, azimuth, AngOmega[i])
+    hora_sol[i] = hora_aparec_sol + (hora_puesta_sol - hora_aparec_sol) / nn * (i - 1)
+    hora_std[i] = m.standard_time(n, hora_sol[i], longitud_local)
+    ang_omega[i] = m.hour_angle(hora_sol[i])
+    ang_theta[i] = m.incidence_angle(ang_delta, latitud_local, inclinacion, azimuth, ang_omega[i])
+    ang_theta_z[i] = m.zenith_angle(ang_delta, latitud_local, azimuth, ang_omega[i])
 
   # CALCULO DE LA RADIACION HORARIA EXTRATERRESTRE HORIZONTAL (en el n dia) [W/m2]
   # ========================================================
   Go_m = np.zeros(nn)
 
   for i in range(nn):
-      Go_m[i] = m.extraterrestrial_horizontal_radiation(Gon, AngTheta[i])
+      Go_m[i] = m.extraterrestrial_horizontal_radiation(gon, ang_theta[i])
 
   # ESTIMACION DE LA RADIACION HORARIA DE HAZ Y DIFUSA EN CIELO DESPEJADO EN DIRECCION DEL SOL G_BEAM_n(Duffie, 2023)
   # ==============================================================================
-  TAU_BEAM = np.zeros(nn)
-  G_BEAMn = np.zeros(nn)
-  TAU_DIF = np.zeros(nn)
-  G_OO = np.zeros(nn)
-  G_DIFUS = np.zeros(nn)
+  tau_beam = np.zeros(nn)
+  g_beam_n = np.zeros(nn)
+  tau_dif = np.zeros(nn)
+  g_oo = np.zeros(nn)
+  g_difus = np.zeros(nn)
 
   for i in range(nn):
-      TAU_BEAM[i] = m.sky_transmissivity(AngThetaZ[i], altitud_local)
-      G_BEAMn[i] = m.beam_radiation(Gon, TAU_BEAM[i])
-      TAU_DIF[i] = m.diffuse_transmissivity(TAU_BEAM[i])
-      G_OO[i] = m.extraterrestrial_horizontal_radiation(Gon, AngThetaZ[i])
-      G_DIFUS[i] = m.diffuse_radiation_horizontal(G_OO[i], TAU_DIF[i])
+    tau_beam[i] = m.sky_transmissivity(ang_theta_z[i], altitud_local)
+    g_beam_n[i] = m.beam_radiation(gon, tau_beam[i])
+    tau_dif[i] = m.diffuse_transmissivity(tau_beam[i])
+    g_oo[i] = m.extraterrestrial_horizontal_radiation(gon, ang_theta_z[i])
+    g_difus[i] = m.diffuse_radiation_horizontal(g_oo[i], tau_dif[i])
 
-  F_forma = m.diffuse_radiation_shape_function(D_int, D_ext, S_sep)
+  F_forma = m.diffuse_radiation_shape_function(d_int, d_ext, s_sep)
 
   # CALCULO DE LAS POTENCIAS HORARIAS QUE INCIDEN SOBRE UN TUBO AL VACIO [W] (Tang, 2009)
   # =========================================================================
-  Nx = np.zeros(nn)
-  Ny = np.zeros(nn)
-  Nz = np.zeros(nn)
-  NNx = np.zeros(nn)
-  NNy = np.zeros(nn)
-  NNz = np.zeros(nn)
-  ANGULO_OMEGA = np.zeros(nn)
-  AngThetaT = np.zeros(nn)
-  FUNC_ACCEP = np.zeros(nn)
-  POTENCIA_HAZ_1T = np.zeros(nn)
-  G_DIFUS_BETA = np.zeros(nn)
-  POTENCIA_DIFUS_1T = np.zeros(nn)
+  nx = np.zeros(nn)
+  ny = np.zeros(nn)
+  nz = np.zeros(nn)
+  nnx = np.zeros(nn)
+  nny = np.zeros(nn)
+  nnz = np.zeros(nn)
+  angulo_omega = np.zeros(nn)
+  ang_theta_t = np.zeros(nn)
+  func_accep = np.zeros(nn)
+  potencia_haz_1t = np.zeros(nn)
+  g_difus_beta = np.zeros(nn)
+  potencia_difus_1t = np.zeros(nn)
 
   for i in range(nn):
-      Nx[i], Ny[i], Nz[i] = m.sun_position(ang_delta, latitud_local, AngOmega[i])
-      NNx[i], NNy[i], NNz[i] = m.sun_position_prima(Nx[i], Ny[i], Nz[i], inclinacion, azimuth)
-      AngThetaT[i] = np.degrees(math.acos(math.sqrt(NNx[i]**2 + NNy[i]**2)))
-      FUNC_ACCEP[i] = m.acceptance_function(D_int, D_ext, S_sep, ANGULO_OMEGA[i])
-      POTENCIA_HAZ_1T[i] = m.direct_radiant_power(G_BEAMn[i], D_int, L_tubo, AngThetaT[i], FUNC_ACCEP[i])
-      G_DIFUS_BETA[i] = m.diffuse_radiation_inclined_surface(G_DIFUS[i], inclinacion)
-      POTENCIA_DIFUS_1T[i] = m.diffuse_radiant_power(G_DIFUS_BETA[i], D_int, L_tubo, F_forma)
+    nx[i], ny[i], nz[i] = m.sun_position(ang_delta, latitud_local, ang_omega[i])
+    nnx[i], nny[i], nnz[i] = m.sun_position_prima(nx[i], ny[i], nz[i], inclinacion, azimuth)
+    ang_theta_t[i] = np.degrees(math.acos(math.sqrt(nnx[i]**2 + nny[i]**2)))
+    func_accep[i] = m.acceptance_function(d_int, d_ext, s_sep, angulo_omega[i])
+    potencia_haz_1t[i] = m.direct_radiant_power(g_beam_n[i], d_int, l_tubo, ang_theta_t[i], func_accep[i])
+    g_difus_beta[i] = m.diffuse_radiation_inclined_surface(g_difus[i], inclinacion)
+    potencia_difus_1t[i] = m.diffuse_radiant_power(g_difus_beta[i], d_int, l_tubo, F_forma)
 
-  POTENCIA_TOTAL_1T = POTENCIA_HAZ_1T + POTENCIA_DIFUS_1T
+  potencia_total_1t = potencia_haz_1t + potencia_difus_1t
 
-  Energia_Total_1T = np.trapz(HoraStd, POTENCIA_TOTAL_1T) / 1000
-
-  return Energia_Total_1T
+  return np.trapz(hora_std, potencia_total_1t) / 1000
